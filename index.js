4e370bdcc76bff59af3ddb0cd57ba3643e0c5449
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-04-10 13:23:39 +0100

    Removed GitHub token from package.json

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 
 const replaceTemplate = require("./modules/replaceTemplate");
 
@@ -48,41 +49,34 @@ console.log("Calm down jare");
 /////////////////////////////////
 //// SERVERS
 
-const tempOverview = fs.readFileSync(
-  `${__dirname}/templates/template-overview.html`,
-  "utf-8"
-);
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
 
-const tempCard = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  "utf-8"
-);
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
 
-const tempProduct = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  "utf-8"
-);
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, {lower: true}));
+
+console.log(slugs);
 
 // console.log(dataObj);
 
 const server = http.createServer((req, res) => {
   // console.log(res);
 
-  const { query, pathname: pathName } = url.parse(req.url, true);
+  const {query, pathname: pathName} = url.parse(req.url, true);
 
   // const pathName = req.url;
 
   //Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.writeHead(200, { "Content-type": "text/html" });
+    res.writeHead(200, {"Content-type": "text/html"});
 
-    const cardsHTML = dataObj
-      .map((el) => replaceTemplate(tempCard, el))
-      .join("");
+    const cardsHTML = dataObj.map((el) => replaceTemplate(tempCard, el)).join("");
 
     const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHTML);
 
@@ -90,7 +84,7 @@ const server = http.createServer((req, res) => {
   }
   //Product page
   else if (pathName === "/product") {
-    res.writeHead(200, { "Content-type": "text/html" });
+    res.writeHead(200, {"Content-type": "text/html"});
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
 
